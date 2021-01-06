a3662eaa1ed832535e1a207183c8fccbe1be7736
Sasini <63692107+Sasini2020@users.noreply.github.com>
2021-01-06 11:26:14 +0530

    updated

--- Email_Verification_in_Real_domain/VerifyEmail.class.php
+++ Email_Verification_in_Real_domain/VerifyEmail.class.php
@@ -2,9 +2,6 @@
 /** 
  * Class to validate the email address 
  * 
- * @author CodexWorld.com <contact@codexworld.com> 
- * @copyright Copyright (c) 2018, CodexWorld.com
- * @url https://www.codexworld.com
  */ 
 class VerifyEmail { 
 
